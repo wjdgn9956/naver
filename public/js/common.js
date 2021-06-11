9520cf789de129dfb8de34814e571f1eb9b33646
hobit22 <hobit22@naver.com>
2021-06-11 16:08:45 +0900

    deco

---
+++ public/js/common.js
@@ -0,0 +1,19 @@
+/** swiper 영역 */
+window.addEventListener("DOMContentLoaded",function(e){
+
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+          el: '.swiper-pagination',
+          clickable : true,
+        },
+      loop:true,
+      autoplay:{
+          delay:5000,
+      }
+      
+    });
+},false);
